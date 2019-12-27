e228eaba0d79a918537491a1beb2ae43fda1c311
Abhishek0706 <apatil@cs.iitr.ac.in>
2019-12-27 17:25:48 +0530

    modified example

--- app/src/main/java/com/example/secretlocklibrary/MainActivity.java
+++ app/src/main/java/com/example/secretlocklibrary/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.secretlocklibrary;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,16 +14,66 @@ import com.example.secretlock.SecretLock;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler = new Handler();
+    private int interval = 1000;
+    Button button;
+    TextView textView;
+    LinearLayout ll;
+
+    final SecretLock secretLock = new SecretLock();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.text);
+        ll = findViewById(R.id.layout);
 
-        Intent i = new Intent(this, NextActivity.class);
+        startRepeatingTask();
 
-        if (new SecretLock().getLockValue(getApplicationContext())) {
-            startActivity(i);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                secretLock.openSettings(MainActivity.this);
+            }
+        });
+
+
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            if (secretLock.getLockValue(getApplicationContext())) {
+                button.setVisibility(View.VISIBLE);
+                textView.setText("Unlocked");
+                ll.setBackgroundColor(Color.BLUE);
+            } else {
+                button.setVisibility(View.INVISIBLE);
+                textView.setText("Locked");
+                ll.setBackgroundColor(Color.RED);
+
+            }
+            handler.postDelayed(mStatusChecker, interval);
         }
+    };
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+
+    private void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    private void stopRepeatingTask() {
+        handler.removeCallbacks(mStatusChecker);
     }
 }
